73451334c0f5f714c5fbd29ad535751555f68da8
Bent Bracke <bent@bracke.dk>
2017-07-28 19:37:19 +0200

    Initial

---
+++ config/environment.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(environment) {
+  let ENV = {
+    modulePrefix: 'timers',
+    environment: environment
+  };
+
+  return ENV;
+};
---
+++ src/ui/components/timers-app/component.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class Timers extends Component {
+
+}
